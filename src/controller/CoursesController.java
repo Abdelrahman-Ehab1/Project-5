b23b5a16e214297c7ee1906abe011daa54279ad5
mohamednasser777 <mohamednasser5228@gmail.com>
2025-11-17 00:49:13 +0200

    course controller 1

--- src/controller/CoursesController.java
+++ src/controller/CoursesController.java
@@ -1,12 +1,20 @@
 package controller;
 
 import database.CoursesDatabase;
+import database.Database;
 import models.Course;
+import models.Student;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class CoursesController {
     private CoursesDatabase coursesDB;
+    private Database usersDB;
+    public CoursesController(CoursesDatabase coursesDB , Database usersDB) {
+        this.coursesDB = coursesDB;
+        this.usersDB = usersDB;
+    }
 
     public boolean updateCourse(Course updatedCourse) {
         for (int i = 0; i <coursesDB.getAllCourses().size(); i++) {
@@ -41,5 +49,43 @@ public class CoursesController {
 //    public boolean courseExists(String courseId) {
 //        return coursesDB.getCourseById(courseId) != null;
 //    }
+
+
+    void addStudentToCourse(String studentId ,String courseId){
+        Course wantedCourse = coursesDB.getCourseById(courseId);
+        if(wantedCourse.getStudentIds().contains(studentId))
+            throw new IllegalArgumentException("student already enrolled in that course");
+
+        wantedCourse.getStudentIds().add(studentId);
+        Student student = (Student) usersDB.findById(studentId);
+        student.getEnrolledCourses().add(wantedCourse);
+
+        coursesDB.saveCourses();
+        usersDB.saveUsers();
+    }
+
+    void removeStudentFromCourse(String studentId ,String courseId){
+        Course wantedCourse = coursesDB.getCourseById(courseId);
+        wantedCourse.getStudentIds().remove(studentId);           //that student removed from that course
+
+        Student student = (Student) usersDB.findById(studentId);
+        student.getEnrolledCourses().remove(wantedCourse);
+
+        coursesDB.saveCourses();
+        usersDB.saveUsers();
+    }
+
+    List<String> getEnrolledStudentIds(String courseId){
+        Course wantedCourse = coursesDB.getCourseById(courseId);
+        return wantedCourse.getStudentIds();
+        //no need to save we're just viewing what's inside
+    }
+
+    void addLesson(String courseId, Lesson lesson)
+    void updateLesson(String courseId, Lesson updatedLesson)
+    void deleteLesson(String courseId, String lessonId)
+    List<Lesson> getLessons(String courseId)
+
+
 }
 
