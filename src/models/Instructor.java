68679bdb2a290078f213003991da9e57739ab039
Abdelrahman Mohamed <abdelrahman20205050@gmail.com>
2025-11-17 02:31:10 +0200

    Student and instructor removed

--- src/models/Instructor.java
+++
@@ -1,7 +0,0 @@
-package models;
-
-public class Instructor extends User {
-    public Instructor(String username, String email, String passwordHash) {
-        super(username, email, passwordHash, "INSTRUCTOR");
-    }
-}
