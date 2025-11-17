2fb01052c6447b222648b5ed47f6e304945d184e
MohamedHany25 <mohamedhany25250@gmail.com>
2025-11-17 02:33:33 +0200

    studentttt

---
+++ src/models/Student.java
@@ -0,0 +1,36 @@
+package models;
+
+import controller.CoursesController;
+
+import java.util.ArrayList;
+
+public class Student extends User {
+   private ArrayList <Course> enrolledCourses; // courses that the student in
+   private ArrayList <Lesson> progress;
+   public Student(String username, String email, String passwordHash){
+      super(username, email, passwordHash, "STUDENT");
+   }
+
+
+    public ArrayList<Course> getEnrolledCourses() {
+      return enrolledCourses;
+   }
+
+    public void setEnrolledCourses(ArrayList<Course> enrolledCourses) {
+        this.enrolledCourses = enrolledCourses;
+    }
+
+    public ArrayList<Lesson> getProgress() {
+        return progress;
+    }
+
+    public void setProgress(ArrayList<Lesson> progress) {
+        this.progress = progress;
+    }
+
+
+   void BrowseAvailableCourses(){
+
+   }
+
+}
