e80df09743eeaa9d6121105672a4548bc6e5a500
mohamednasser777 <mohamednasser5228@gmail.com>
2025-11-22 03:17:35 +0200

    quiz 3

--- src/controller/QuizController.java
+++ src/controller/QuizController.java
@@ -1,7 +1,11 @@
 package controller;
 
+import models.Question;
 import models.Quiz;
 import models.QuizProgress;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class QuizController {
     private Quiz quiz;
@@ -25,9 +29,40 @@ public class QuizController {
         return lessonId;
     }
 
-    public void addOption(String option){
+    /*public void addOption(String option , int number){
         if(option == null || option.isEmpty())
             throw new IllegalArgumentException("option can not be empty");
-        quiz.getoptions.add(option);
+        quiz.getQuestionByNumber(number).getOptions().add(option);
+    }*/
+
+    public boolean answerQuestion(int choice, int index){
+
+        return choice == index;
     }
+
+    public void addQuestion(String text , List<String> options ,int answerIndex ){
+        Question question = new Question(text,options,answerIndex);
+        quiz.addQuestion(question);
+    }
+    public void setQuestionsOfQuiz(){
+        addQuestion("what is the capital of Egypt?", Arrays.asList("Alexandria","Cairo","Aswan","New York"), 1);
+        addQuestion("what is the capital of France?", Arrays.asList("Paris","Cairo","Oslo","New York"), 0);
+        addQuestion("what is the capital of Italy?", Arrays.asList("Alexandria","Cairo","Aswan","Rome"), 3);
+        addQuestion("what is the capital of Germany?", Arrays.asList("Jeddah","Berlin","Aswan","New York"), 1);
+        addQuestion("what is the capital of Japan?", Arrays.asList("Cairo","Istanbul","Tokyo","New York"), 2);
+    };
+
+    public void answerQuizQuestions(List<Integer> answers){
+        quizProgress.addAttempt();
+        int size = quiz.getAllQuestions().size();
+
+        for(int i =0;i<size;i++){
+            Question question = quiz.getAllQuestions().get(i);
+            if(question.getCorrectAnswer() == answers.get(i))
+
+
+
+        }
+    }
+
 }
